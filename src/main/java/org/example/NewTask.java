4ad2be1300640b76122b476f789742e7d8b4d1dd
Mikal <mdr015@uib.no>
2023-10-21 14:54:46 +0200

    continued the task

---
+++ src/main/java/org/example/NewTask.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.nio.charset.StandardCharsets;
+
+public class NewTask {
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        String message = String.join(" ", argv);
+
+        channel.basicPublish("", "hello", null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
